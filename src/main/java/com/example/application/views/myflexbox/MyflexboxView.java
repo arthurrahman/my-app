39d7e623b10f582d7fcb77e7063ca389f2eed811
Arthur Shofiqur Rahman <arthur.rahman@compax.at>
2025-01-27 16:27:36 +0100

    combo boxes added to view

--- src/main/java/com/example/application/views/myflexbox/MyflexboxView.java
+++ src/main/java/com/example/application/views/myflexbox/MyflexboxView.java
@@ -9,8 +9,14 @@ import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
 import com.opencsv.exceptions.CsvException;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.grid.GridVariant;
 import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.NotificationVariant;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Menu;
 import com.vaadin.flow.router.PageTitle;
@@ -22,7 +28,9 @@ import org.vaadin.lineawesome.LineAwesomeIconUrl;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @PageTitle("myflexbox")
 @Route("")
@@ -31,22 +39,77 @@ public class MyflexboxView extends VerticalLayout {
 
     @Autowired
     private UserRepository userRepository;
+    private String[] headers;
+    private Map<String, String> fieldMappings = new HashMap<>();
 
     public MyflexboxView() {
         VerticalLayout mainLayout = new VerticalLayout();
         mainLayout.setSpacing(false);
         mainLayout.getThemeList().add("spacing-xs");
 
-        H2 header = new H2("CSV DATA TABLE");
+        H2 tableHeader = new H2("CSV DATA TABLE");
+        tableHeader.getStyle().set("color", "#154360");
+        H2 comboBoxHeader = new H2("Map columns and save input Data");
+        comboBoxHeader.getStyle().set("color", "#154360");
+        comboBoxHeader.getStyle().set("padding-left", "100px");
+
         // load table data from input.csv file
-        Grid<String[]> grid = importTableData();
+        Grid<String[]> dataTable = importTableData();
+        
+        // add combo boxes
+        VerticalLayout comboBoxLayout = addComboBoxes();
 
-        Button saveButton = new Button("Save", event -> saveCSVData());
+        Button saveButton = new Button("Save to Database", event -> {
+            saveCSVData();
 
-        mainLayout.add(header);
-        mainLayout.add(grid);
+            Notification notification = new Notification("Input data was saved to the database!");
+            notification.setPosition(Notification.Position.TOP_END);
+            notification.addThemeVariants(NotificationVariant.LUMO_SUCCESS);;
+            notification.setDuration(3000);
+            notification.open();
+        });
+        saveButton.setWidth("600px");
+        saveButton.getStyle().set("padding-top", "-10px");
+        saveButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+
+        mainLayout.add(tableHeader);
+        mainLayout.add(dataTable);
+        mainLayout.add(comboBoxHeader);
+        mainLayout.add(comboBoxLayout);
         mainLayout.add(saveButton);
+        mainLayout.setSpacing(true);
         add(mainLayout);
+    }
+
+    private VerticalLayout addComboBoxes() {
+        VerticalLayout verticalLayout = new VerticalLayout();
+        HorizontalLayout horizontalLayout = new HorizontalLayout();
+
+        for (int i = 0; i < headers.length; i++) {
+            final int columnIndex = i;
+            ComboBox<String> comboBox = new ComboBox<>(headers[i]);
+            comboBox.setItems("first", "last", "address", "zip", "country", "ignore");
+            comboBox.setValue("first");
+
+            comboBox.addValueChangeListener(event -> {
+                fieldMappings.put(headers[columnIndex], event.getValue()); // Store mapping in fieldMappings map
+            });
+
+            comboBox.getStyle().set("margin-left", "-15px");
+            comboBox.getStyle().set("margin-top", "-10px");
+            comboBox.getStyle().set("padding-left", "-15px");
+            comboBox.getStyle().set("padding-right", "10px");
+            comboBox.getStyle().set("padding-top", "-10px");
+
+            horizontalLayout.add(comboBox);
+
+            if ((i + 1) % 3 == 0 || i == headers.length - 1) {
+                verticalLayout.add(horizontalLayout);
+                horizontalLayout = new HorizontalLayout();
+            }
+        }
+
+        return verticalLayout;
     }
 
     Grid<String[]> importTableData() {
@@ -62,19 +125,25 @@ public class MyflexboxView extends VerticalLayout {
         try {
             List<String[]> entries = reader.readAll();
             // Assume the first row contains headers
-            String[] headers = entries.get(0);
+            headers = entries.get(0);
 
             // Setup a grid with random data
             for (int i = 0; i < headers.length; i++) {
                 final int columnIndex = i;
                 String header = headers[i];
                 String humanReadableHeader = SharedUtil.camelCaseToHumanFriendly(header);
-                grid.addColumn(str -> str[columnIndex]).setHeader(humanReadableHeader);
+                grid.addColumn(str -> str[columnIndex])
+                        .setHeader(humanReadableHeader)
+                        .setAutoWidth(true)
+                        .setResizable(true);
             }
             grid.setItems(entries.subList(1, entries.size()));
         } catch (IOException | CsvException e) {
             grid.addColumn(nop -> "Unable to load CSV: " + e.getMessage()).setHeader("Failed to import CSV file");
         }
+        grid.setWidth("60%");
+        grid.addThemeVariants(GridVariant.LUMO_ROW_STRIPES);
+
         return grid;
     }
 
@@ -91,5 +160,6 @@ public class MyflexboxView extends VerticalLayout {
         user.setAddress(address);
 
         userRepository.save(user);
+
     }
 }
